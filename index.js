295233b2c982a94081f83fe744c443fa13f39ad9
manoelkemtsa800-cyber <manoelkemtsa800@gmail.com>
2026-06-21 01:02:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Alert} from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './App';
 import {name as appName} from './app.json';
+
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  Alert.alert(
+    isFatal ? 'Erreur fatale' : 'Erreur',
+    `${error.name}: ${error.message}\n\n${error.stack}`,
+    [{text: 'OK'}],
+  );
+});
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => require('./service'));
